e09bc9bea09c8ed11bc485f0e8a3560267748f98
Valkon <96625039+Nawija@users.noreply.github.com>
2023-09-18 18:35:41 +0200

    nn

--- src/pages/navigation/index.js
+++ src/pages/navigation/index.js
@@ -63,7 +63,7 @@ export default function NavigationPage() {
                         />
                     </div>
                 </div>
-            ))}
+            ))} 
         </Layout>
     );
 }
