0576bc4d767ad55e30bd3fe5ba15a6f7529edf53
Valkon <96625039+Nawija@users.noreply.github.com>
2023-09-19 11:48:35 +0200

    Update index.js

--- src/pages/navigation/index.js
+++ src/pages/navigation/index.js
@@ -30,35 +30,35 @@ export default function NavigationPage() {
 
     return (
         <Layout>
-        {data.allDatoCmsNav.edges.map(({ node }, index) => (
-            <div className="relative mb-2 group" key={index}>
-                <div className="flex-ec w-full text-xl p-2">
-                    <button
-                        className="bg-main p-3 rounded-full border text-white"
-                        onClick={() => handleCopyClick(index)}
-                    >
-                        {copiedIndex === index ? (
-                            <div className="text-green-600 relative font-semibold scale-up-center">
-                                <FaCheck />
-                                <span
-                                    className={`text-sm absolute top-1/2 -left-5 rounded-lg transition-transform -translate-x-full -translate-y-1/2 bg-white p-2`}
-                                >
-                                    Skopiowano!
-                                    <div className="bg-white w-2 h-2 -z-10 rotate-45 absolute top-1/2 right-0 -translate-y-1/2 translate-x-1/2" />
-                                </span>
-                            </div>
-                        ) : (
-                            <FaCopy />
-                        )}
-                    </button>
+            {data.allDatoCmsNav.edges.map(({ node }, index) => (
+                <div className="relative mb-2 group" key={index}>
+                    <div className="flex-ec w-full text-xl p-2">
+                        <button
+                            className="bg-main p-3 rounded-full border text-white"
+                            onClick={() => handleCopyClick(index)}
+                        >
+                            {copiedIndex === index ? (
+                                <div className="text-green-600 relative font-semibold scale-up-center">
+                                    <FaCheck />
+                                    <span
+                                        className={`text-sm absolute top-1/2 -left-5 rounded-lg transition-transform -translate-x-full -translate-y-1/2 bg-white p-2`}
+                                    >
+                                        Skopiowano!
+                                        <div className="bg-white w-2 h-2 -z-10 rotate-45 absolute top-1/2 right-0 -translate-y-1/2 translate-x-1/2" />
+                                    </span>
+                                </div>
+                            ) : (
+                                <FaCopy />
+                            )}
+                        </button>
+                    </div>
+                    <div className="bg-white rounded-lg border m-2">
+                        <div dangerouslySetInnerHTML={{ __html: node.code }} />
+                    </div>
                 </div>
-                <div className="bg-white rounded-lg border m-2">
-                    <div dangerouslySetInnerHTML={{ __html: node.code }} />
-                </div>
-            </div>
-        ))}
+            ))}
 
-        
-    </Layout>
+            
+        </Layout>
     );
 }
