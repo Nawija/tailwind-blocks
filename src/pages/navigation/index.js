ef47617930425931e7990d15b3f51e73bc334634
Valkon <96625039+Nawija@users.noreply.github.com>
2023-09-19 09:30:56 +0200

    dd

--- src/pages/navigation/index.js
+++ src/pages/navigation/index.js
@@ -1,27 +1,17 @@
-import React, { useRef, useState } from "react";
-import { useStaticQuery, graphql } from "gatsby";
+import React, { useState } from "react";
 import Layout from "../../components/layout";
 import { FaCopy, FaCheck } from "react-icons/fa6";
 
-export default function NavigationPage() {
-    const data = useStaticQuery(graphql`
-        {
-            allDatoCmsNav {
-                edges {
-                    node {
-                        code
-                        tag
-                    }
-                }
-            }
-        }
-    `);
-    const [copiedIndex, setCopiedIndex] = useState(-1);
+import A from "../../components/navigation/a";
+import B from "../../components/navigation/b";
 
-    const handleCopyClick = (index) => {
-        const htmlToCopy = data.allDatoCmsNav.edges[index].node.code;
+export default function NavigationPage() {
+    const [copiedIndex, setCopiedIndex] = useState(-1);
+    const [htmlToCopy, setHtmlToCopy] = useState("");
+
+    const handleCopyClick = () => {
         navigator.clipboard.writeText(htmlToCopy).then(() => {
-            setCopiedIndex(index);
+            setCopiedIndex(0); // Przykładowy index lub zmiennej związaną z kontekstem
             setTimeout(() => {
                 setCopiedIndex(-1);
             }, 1000);
@@ -30,14 +20,13 @@ export default function NavigationPage() {
 
     return (
         <Layout>
-        {data.allDatoCmsNav.edges.map(({ node }, index) => (
-            <div className="relative mb-2 group" key={index}>
+            <div className="relative mb-2 group">
                 <div className="flex-ec w-full text-xl p-2">
                     <button
                         className="bg-main p-3 rounded-full border text-white"
-                        onClick={() => handleCopyClick(index)}
+                        onClick={() => handleCopyClick()}
                     >
-                        {copiedIndex === index ? (
+                        {copiedIndex !== -1 ? (
                             <div className="text-green-600 relative font-semibold scale-up-center">
                                 <FaCheck />
                                 <span
@@ -53,12 +42,36 @@ export default function NavigationPage() {
                     </button>
                 </div>
                 <div className="bg-white rounded-lg border m-2">
-                    <div dangerouslySetInnerHTML={{ __html: node.code }} />
+                    <A setHtmlToCopy={setHtmlToCopy} />{" "}
+                    {/* Przekaż funkcję do ustawiania zawartości */}
                 </div>
             </div>
-        ))}
-
-        
-    </Layout>
+            <div className="relative mb-2 group">
+                <div className="flex-ec w-full text-xl p-2">
+                    <button
+                        className="bg-main p-3 rounded-full border text-white"
+                        onClick={() => handleCopyClick()}
+                    >
+                        {copiedIndex !== -1 ? (
+                            <div className="text-green-600 relative font-semibold scale-up-center">
+                                <FaCheck />
+                                <span
+                                    className={`text-sm absolute top-1/2 -left-5 rounded-lg transition-transform -translate-x-full -translate-y-1/2 bg-white p-2`}
+                                >
+                                    Skopiowano!
+                                    <div className="bg-white w-2 h-2 -z-10 rotate-45 absolute top-1/2 right-0 -translate-y-1/2 translate-x-1/2" />
+                                </span>
+                            </div>
+                        ) : (
+                            <FaCopy />
+                        )}
+                    </button>
+                </div>
+                <div className="bg-white rounded-lg border m-2">
+                    <B setHtmlToCopy={setHtmlToCopy} />{" "}
+                    {/* Przekaż funkcję do ustawiania zawartości */}
+                </div>
+            </div>
+        </Layout>
     );
 }
