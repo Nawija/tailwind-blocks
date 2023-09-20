fd7f705dc3d805987f862d8faf67ad9c6f3c9294
Valkon <96625039+Nawija@users.noreply.github.com>
2023-09-20 17:53:14 +0200

    dfg

--- src/pages/navigation.js
+++ src/pages/navigation.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Layout from "../../components/layout";
+import Layout from "../components/layout";
 
-import BlockWrapper from "../../components/BlockWrapper";
+import BlockWrapper from "../components/BlockWrapper";
 
-import A from "../../components/navigation/a";
+import A from "../components/navigation/a";
 
 export default function NavigationPage() {
     return (
