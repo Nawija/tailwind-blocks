5e8d2e670b6b658aa2f1d5606df7a0effa5bb54c
Valkon <96625039+Nawija@users.noreply.github.com>
2023-09-23 15:39:04 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,16 +9,17 @@ export default function IndexPage() {
             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 wrapper2xl flex items-center justify-center flex-col font-bold text-3xl text-main w-max">
                 <p className="">Welcome to</p>
                 <div className="flex items-end justify-center flex-col">
-                    <h1 className="text-5xl md:text-6xl w-max lg:text-7xl -tracking-wide p-1 bg-gradient-to-tr from-blue-500 to-blue-700 bg-clip-text text-transparent">
+                    <h1 className="text-5xl md:text-6xl w-max lg:text-7xl -tracking-wide p-1 bg-gradient-to-tr from-blue-400 to-blue-700 bg-clip-text text-transparent">
                         Tailwind Blocks
                     </h1>
                     <p className="text-sm">
                         by{" "}
                         <Link
                             to="http://seovileo.pl/"
-                            className="bg-gradient-to-r from-orange-600 to-yellow-500 bg-clip-text text-transparent -tracking-wide"
+                            target="_blank"
+                            className="bg-gradient-to-r from-orange-600 to-amber-400 bg-clip-text text-transparent -tracking-wide"
                         >
-                            seovileo.pl
+                            seovileo
                         </Link>
                     </p>
                 </div>
