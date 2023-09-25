f5fc175e3f865e935fc280081fdc0d76da83437e
Valkon <96625039+Nawija@users.noreply.github.com>
2023-09-25 20:55:12 +0200

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,5 +8,5 @@
  * @type {import('gatsby').GatsbySSR['onRenderBody']}
  */
 exports.onRenderBody = ({ setHtmlAttributes }) => {
-    setHtmlAttributes({ lang: `pl` });
+    setHtmlAttributes({ lang: `en` });
 };
