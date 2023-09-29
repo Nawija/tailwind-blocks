623c787f17f7a7a7992dfa1355028296161c51bc
Valkon <96625039+Nawija@users.noreply.github.com>
2023-09-29 11:57:05 +0200

    Update ShareMenu.js

--- src/components/ShareMenu.js
+++ src/components/ShareMenu.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { IoCloseOutline } from "react-icons/io5";
+import { FaFacebookF, FaXTwitter } from "react-icons/fa6";
 
 const styleBtn =
-    "border border-white p-2 m-2 rounded-xl hover:bg-white/90 hover:text-blue-700 font-medium transition-colors ";
+    "flex items-center justify-start border border-white p-2 m-2 rounded-xl hover:bg-white/90 hover:text-blue-700 font-medium transition-colors ";
 
 function ShareMenu({ shareMenu, handleShareMenuClose }) {
     const [addClassAnim, setAddClassAnim] = useState(false);
@@ -17,17 +18,11 @@ function ShareMenu({ shareMenu, handleShareMenuClose }) {
 
     const handleClick = (platform) => {
         let shareUrl = "";
-
-        // Define the share URL based on the selected platform
         switch (platform) {
             case "facebook":
                 shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
                     window.location.href
                 )}`;
-                break;
-            case "instagram":
-                // Replace 'YOUR_INSTAGRAM_SHARE_URL' with the actual Instagram share URL for your website
-                shareUrl = "YOUR_INSTAGRAM_SHARE_URL";
                 break;
             case "twitter":
                 shareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(
@@ -45,7 +40,7 @@ function ShareMenu({ shareMenu, handleShareMenuClose }) {
     return (
         shareMenu && (
             <div
-                className={`bg-blue-500 fixed right-0 top-20 -translate-x-1/2  -translate-y-1/2 p-12 w-96 h-96 z-[99] slide-bl text-white rounded-lg rounded-tr-none flex items-center justify-center flex-col shadow-2xl ${
+                className={`bg-blue-600/90 backdrop-blur-md fixed right-0 top-20 -translate-x-1/2  -translate-y-1/2 p-12 w-80 h-96 z-[99] slide-bl text-white rounded-lg rounded-tr-none flex items-center justify-center flex-col shadow-2xl ${
                     addClassAnim ? "slide-tr" : ""
                 }`}
             >
@@ -55,25 +50,21 @@ function ShareMenu({ shareMenu, handleShareMenuClose }) {
                 >
                     <IoCloseOutline className="text-white text-3xl" />
                 </button>
-                <h2 className="mb-12 font-medium text-xl">Udostepnij nas:</h2>
+                <h2 className="mb-12 font-medium text-xl">Social Media:</h2>
                 <div className="flex items-stretch justify-center flex-col text-center">
                     <button
                         onClick={() => handleClick("facebook")}
                         className={styleBtn}
                     >
+                        <FaFacebookF className="mr-2" />
                         Facebook
-                    </button>
-                    <button
-                        onClick={() => handleClick("instagram")}
-                        className={styleBtn}
-                    >
-                        Instagram
                     </button>
                     <button
                         onClick={() => handleClick("twitter")}
                         className={styleBtn}
                     >
-                        Twitter
+                        <FaXTwitter className="mr-2" />
+                        X-Twitter
                     </button>
                 </div>
             </div>
